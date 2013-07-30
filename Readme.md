52d16719291b9830677ba4777ad134b3c1dfc0bd
emmasteimann <emmasteimann@gmail.com>
2013-07-29 22:23:12 -0700

    Myst Comic Downloader.

---
+++ Readme.md
@@ -0,0 +1,21 @@
+Myst Book of Atrus Downloader
+=========
+
+Purpose....
+-----------
+I made my original comic downloader for Scary Go Round, but then I wondered if I could expand it to my other favorite web comics, so I've now expanded it to [Myst: The Book of Atrus](http://mystcomic.smackjeeves.com/).
+
+Each chapter is created every 25 comics and loaded into its own `.cbz` file. Which can be used with most comic readers. For Mac, see [Simple Comic](http://dancingtortoise.com/simplecomic/) or [Jomic](http://jomic.sourceforge.net/).
+
+How to use...
+-----------
+A downloader tool for my favorite webcomic.
+
+  - Run `bundle`
+  - Run `bundle exec ruby getMyst.rb`
+  - Enjoy all of Myst!
+
+What this does....
+-----------
+
+This downloads the entirety of XKCD from 2012-Present, where Heather Larkin. has now posted 130+ comics.
---
+++ getMyst.rb
@@ -0,0 +1,85 @@
+require 'mechanize'
+require 'logger'
+require 'pry'
+require 'zip/zip'
+
+class GetMYST
+  attr_accessor :agent, :first_page, :chapter_markers
+
+  def initialize()
+    @agent = Mechanize.new
+    @agent.log = Logger.new "mech.log"
+    @agent.user_agent_alias = 'Mac Safari'
+  end
+
+  def start_download
+    # Start with first page of first Chapter
+    page = @agent.get("http://mystcomic.smackjeeves.com/comics/1432774/page-1/")
+    puts @agent.current_page().uri()
+
+    keep_going = true
+    current_chapter = 1
+    page_cursor = 1
+    while keep_going
+      page = @agent.current_page()
+      if page_cursor % 25 == 0
+        zip_previous_chapter(current_chapter)
+        current_chapter = current_chapter + 1
+        puts "Chapter switched to -> " + current_chapter.to_s
+      end
+
+      comic_url = page.search("//img[contains(@src,'comics')]").first.attributes["src"].to_s
+      image_name = comic_url.split('/')[-1]
+      @agent.get("#{comic_url}").save("#{chapter_directory(current_chapter)}/#{page_cursor}_#{image_name}")
+      puts "Currently Downloading: #{current_chapter}"
+      puts "Downloading comic address: #{comic_url}"
+
+      next_link = page.link_with(:text => "next >")
+      if next_link.href.include? "#"
+        zip_previous_chapter(current_chapter)
+        keep_going = false
+      else
+        next_page = @agent.click(next_link)
+        puts @agent.current_page().uri()
+      end
+      page_cursor = page_cursor + 1
+    end
+  end
+
+  def chapter_directory(chapter_id)
+    "myst_comics/myst_chapter_#{chapter_id}"
+  end
+
+  def zip_file_path(chapter_id)
+    directory_name = "saved_comics"
+    unless File.directory?(directory_name)
+      FileUtils.mkdir_p(directory_name)
+    end
+    "#{directory_name}/myst_chapter_#{chapter_id}.cbz"
+  end
+
+  def zip_previous_chapter(chapter_id)
+    directory = chapter_directory(chapter_id)
+    zipfile_name = zip_file_path(chapter_id)
+    if File.exist?(zipfile_name)
+      File.delete(zipfile_name)
+    end
+    Zip::ZipFile.open(zipfile_name, 'w') do |zipfile|
+      Dir["#{directory}/**/**"].reject{|f|f==zipfile_name}.each do |file|
+        zipfile.add(file.sub(directory+'/',''),file)
+      end
+    end
+  end
+
+  class << self
+    def new_download()
+      scary_downloader = self.new()
+      scary_downloader.start_download
+    end
+  end
+
+end
+
+GetMYST.new_download()
+
+exit
